d33eb7577182f2733a7d4d1c036a7fa4e204d8f0
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2022-09-04 22:07:06 +0900

    badges

--- js/main.js
+++ js/main.js
@@ -15,3 +15,26 @@ searchInputEl.addEventListener('blur', ()=>{
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+
+// _.throttle() 함수가 한번에 너무 많이 실행되지 않도록 일정한 제한을 걸어줌
+// _.throttle(함수, 시간) -> lodash library
+window.addEventListener('scroll', _.throttle(()=>{
+  console.log(window,scrollY);
+  if(window.scrollY > 500) {
+    //배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션); -> gsap library
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
