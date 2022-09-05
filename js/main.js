2dd715867faba10c947ce840f139658d2c2f8b95
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2022-09-05 18:15:01 +0900

    Reserve Store

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /* main.js */
+
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
@@ -84,7 +85,31 @@ promotionToggleBtn.addEventListener('click', ()=>{ //이벤트 핸들러
     // 숨김 처리
     promotionEl.classList.add('hide');
   } else {
-    //보임 처리
+    // 보임 처리
     promotionEl.classList.remove('hide');
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소(선택자), (지속)시간, 옵션);
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    { // 옵션
+      y: size,
+      repeat: -1, // 무한반복
+      yoyo: true, // 반복 애니메이션
+      ease: Power1.easeInOut, 
+      delay: random(0, delay)
+    }
+  );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
---
+++ js/youtube.js
@@ -0,0 +1,28 @@
+/* youtube.js */ 
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() { // !!함수 이름 바꾸면 안된다!!
+  // <div id="player"></div>
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
+    playerVars: {
+      autoplay: true, // 자동 재생 유무
+      loop: true, // 반복 재생 유무
+      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID
+    },
+    events: {
+      onReady: function (event) {
+        event.target.mute() // 음소거
+      }
+    }
+  });
+}
+
