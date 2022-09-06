33c0e0413a5a3576e5f347e5facbb35352f3e837
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2022-09-07 01:41:32 +0900

    모듈화

--- js/main.js
+++ js/main.js
@@ -1,23 +1,5 @@
 /* main.js */
 
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', ()=>{
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', ()=>{
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', ()=>{
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -153,5 +135,3 @@ spyEls.forEach(function (spyEl) {
 });
 
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
